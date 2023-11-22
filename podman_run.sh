6e379a7f9893cb24af26188ba7cb6b138060ffbd
derpmcherpface <kalle.anka@ankeborg.an>
2023-11-22 18:27:55 +0100

    make ollama cache dir persistent mount

--- podman_run.sh
+++ podman_run.sh
@@ -1,11 +1,13 @@
+# ensure directory mounts exist on host
+mkdir -p ~/.ollama
 podman run -t -d  \
        -v ~/.Xauthority:/root/.Xauthority:Z \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        --net=host \
        -e DISPLAY=$DISPLAY \
-	   -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
-	   -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-	   -e PULSE_SERVER=$PULSE_SERVER \
-	   -v ~/work:/work \
-	   localhost/my-test-container:latest
-#     c7f0e13e6bfc
+       -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+       -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+       -e PULSE_SERVER=$PULSE_SERVER \
+       -v ~/work:/work \
+       -v ~/.ollama:/root/ollama \
+       localhost/my-test-container:latest
